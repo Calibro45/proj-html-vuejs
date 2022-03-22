9a5313776706544707764e663649fc195cad65b8
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-03-22 16:07:45 +0100

    bozza card

--- src/components/EditorialCard.vue
+++ src/components/EditorialCard.vue
@@ -2,11 +2,11 @@
     <div class="card-wrapper">
 
         <figure class="card-image">
-            <img src="../assets/img/news-1.jpg" alt="">
+            <img src="../assets/img/news-1.jpg">
         </figure>
 
         <div class="card-body">
-
+            <p>Lorem ipsum dolor sit amet.</p>
         </div>
 
     </div>
@@ -20,5 +20,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/scss/variabili.scss";
+@import "../assets/scss/mixins.scss";
+
+.card-wrapper {
+    border-radius: 10px;
+    overflow: hidden;
+    position: relative;
+
+    .card-body {
+        color: $gray-nurse;
+        position: absolute;
+        right: 25px;
+        bottom: 35px;
+        left: 25px;
+    }
+}
     
 </style>
