038bcdd8bd070b381bab61cf3d6217839a955943
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-03-23 15:09:13 +0100

    footer credits

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,20 +5,21 @@
         <footerLinks />
 
         <!-- section credits -->
-
+        <footerCredits />
         
     </footer>
 </template>
 
 <script>
 import footerLinks from './FooterLink.vue';
+import footerCredits from './FooterCredits.vue';
 
 export default {
     name: 'mainFooter',
     components: {
         footerLinks,
+        footerCredits,
     }
-    
 }
 </script>
 
---
+++ src/components/FooterCredits.vue
@@ -0,0 +1,46 @@
+<template>
+    <section id="footer-credits">
+        <div class="container">
+
+            <div class="credits">
+                <p>Enjoy the low price. We are trackingany intention of privacy.</p>
+            </div>
+            <div class="credits">
+                <p>&copy; 2020 NEXGEN is proudly powered by <a href="#">Codings</a></p>
+            </div>
+
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'footerCredits',
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/variabili.scss";
+@import "../assets/scss/mixins.scss";
+
+#footer-credits {
+    background-color: $wood-smoke;
+
+    .container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .credits {
+            font-size: 0.75rem;
+            padding: 5px 10px;
+            margin-top: 25px;
+
+            a {
+                color: $blue-lagoon;
+            }
+        }
+    }
+}
+    
+</style>
